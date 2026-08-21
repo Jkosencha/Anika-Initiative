7821face57741fc85bf89c4cd0d09a076f54d499
Daniel Muthui <dannymuthui118@gmail.com>
2026-08-21 10:06:48 +0300

    feat: changed color schema for mpesa button

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -174,7 +174,7 @@ const DonationPage = () => {
                 onClick={() => setDonationMethod("mpesa")}
                 className={`flex-1 py-2 px-4 rounded-lg font-medium transition-all ${
                   donationMethod === "mpesa"
-                    ? "bg-[#E6A15E] text-[#1E1A18] shadow-sm"
+                    ? "bg-[#39ba25] text-[#1E1A18] shadow-sm"
                     : "text-gray-500 hover:text-gray-700"
                 }`}
               >
